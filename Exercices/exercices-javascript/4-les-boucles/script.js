b15d9913267ea3bf71404df49caf7bf487ffa4c0
tshibangu-jeremie <66729840+tshibangu-jeremie@users.noreply.github.com>
2020-06-26 15:11:54 +0200

    javajeremie

--- Exercices/exercices-javascript/4-les-boucles/script.js
+++ Exercices/exercices-javascript/4-les-boucles/script.js
@@ -1,5 +1,26 @@
 // Exercice 1: A l'aide d'une boucle, affiche les chiffres de 0 à 100, place les chiffres dans un tableau que tu nommeras fizzBuzzArray, à l'aide de la méthode .push()
+let newArray = [];
+let iteration = newArray;
+for (let i = 0; i <= 100; i++) {
+  newArray.push(i);
+}
 
 // Exercice 2: Toujours à l'aide d'une boucle, passe sur chaque élément du tableau que tu viens de créer (fizzBuzzArray), si l'élément est divisible par trois affiche "fizz" dans la console, si l'élément est divisible par cinq affiche "buzz", si il est divisible par trois et par cinq affiche "fizzBuzz"
-
-// Exercice 3: A l'aide de l'objet Date (voir: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Date) affiche tous les vendredi 13 de l'année 2020
+newArray.forEach(element => {
+  if (element % 3 === 0 && element % 5 === 0) {
+    console.log('fizzBuzz');
+  } else if (element % 3 == 0) {
+    console.log('fuzz');
+  } else if (Element % 5 === 0) {
+    console.log('buzz');
+  }
+});
+// Exercice 3: A l'aide de l'objet Date (voir: r/docs/Web/JavaScriphttps://developer.mozilla.org/ft/Reference/Objets_globaux/Date) affiche tous les vendredi 13 de l'année 2020
+const datel = new datel(' mars 13');
+// sun mars 13 gmt ...
+const datel = new datel(' novembre 13');
+// sun novembre 13 gmt ...
+console.log(date1 === date2);
+// expecded ouput : faise;
+console.log(date1 - date2);
+// expected ouput : 0.
